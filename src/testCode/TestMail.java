4b30417f8d11039d621476308b1773ad01efd42f
pjxiao <flymanxiao@gmail.com>
2016-12-25 10:17:36 -0500

    this is a test

---
+++ src/testCode/TestMail.java
@@ -0,0 +1,10 @@
+package testCode;
+
+/**
+ * Created by flyboy on 12/24/2016.
+ */
+public class TestMail {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
