fb75f32ac1cd322e77eb31bce810516e0cf4b81f
Xiao <pingjiex@amazon.com>
2016-12-27 11:24:02 -0500

    1st commit from mac

--- src/testCode/TestMail.java
+++ src/testCode/TestMail.java
@@ -4,6 +4,10 @@ package testCode;
  * Created by flyboy on 12/24/2016.
  */
 public class TestMail {
+    /**
+     * This is the main function for execuation
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
